c62d081189d0d74d88dba5fd16f16506286f25be
EdgarCodes <edgar452345@gmail.com>
2024-02-01 19:11:02 -0600

    Made some responsiveness changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,87 @@
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 import "../styles/navbar.css"
+import { useRef, useState } from "react";
 
 function Navbar() {
+    const [isOffScreen, setIsOffScreen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleIsOpen = () =>{
+        if(isOpen != null){setIsOpen(!isOpen);}
+        else{setIsOpen(true)}
+    }
+
+    const getIsOpen = () =>{
+        if(isOpen === null)
+        {
+            return "close";
+        }
+        else if(isOpen === true)
+        {
+            return "nav-sidebar";
+        }
+
+        return "nav-sidebar close";
+    }
+
+    window.onscroll = () =>{
+      if(document.body.scrollTop > 95 || document.documentElement.scrollTop > 95)
+      {
+          setIsOffScreen(true);
+      }
+      else
+      {
+          setIsOffScreen(false);
+      }
+      
+    };
+
     return (
-        <div className="navbar-container">
-            <div className="navbar_links">
-                <Link to="/"><div>Home</div></Link>
+        <div className= {isOffScreen? "navbar-container scroll":"navbar-container"}>
+            <div className="navbar-links">
+                <Link to="/" className={({ isActive }) => (isActive? 'active':'')}><div>Home</div></Link>
 
-                <Link to="/quiz"><div>Take Quiz</div></Link>
+                <Link to="/quiz" className={({ isActive }) => (isActive? 'active':'')}><div>Take Quiz</div></Link>
 
-                <Link to="/quiz"><div>Reviews</div></Link>
+                <Link to="/reviews" className={({ isActive }) => (isActive? 'active':'')}><div>Reviews</div></Link>
             </div>
             
-            <div className="navbar_logo_container">
+            <div className="navbar-logo-container">
                 <Link to="/">
-                    <div className="navbar_logo">
+                    <div className="navbar-logo">
                         <img alt="logo" src="https://storage.googleapis.com/intima-bucket/logo_4.png" />
                     </div>
                 </Link>
             </div>
 
-            <div className="navbar_links navbar_links_right">
-                <Link to="/about"><div>About Us</div></Link>
+            <div className="navbar-links">
+                <Link to="/about" className={({ isActive }) => (isActive? 'active':'')}><div>About Us</div></Link>
                 
-                <Link to="/stats"><div>Statistics</div></Link>
+                <Link to="/stats" className={({ isActive }) => (isActive? 'active':'')}><div>Statistics</div></Link>
 
-                <Link to="/quiz"><div>Contact us</div></Link>
+                <Link to="/contact" className={({ isActive }) => (isActive? 'active':'')}><div>Contact Us</div></Link>
+            </div>
+
+
+            
+
+            {/* For smaller screens */}
+
+            <button className = "navbar-drop-down" onClick={toggleIsOpen}>
+                <i className="fas fa-bars"/>
+            </button>
+
+            <div className = {getIsOpen()}>
+
+                <Link to ="/"  onClick = {() => {setIsOpen(false)}}>Home <div className = "underline"/></Link>
+
+                <Link to ="/quiz" onClick = {() => {setIsOpen(false)}}>Take Quiz<div className = "underline"/></Link>
+
+
+                <Link to ="/stats" onClick = {() => {setIsOpen(false)}}>Statistics<div className = "underline"/></Link>
+                
+                <Link to ="/about" onClick = {() => {setIsOpen(false)}}>About Us<div className = "underline"/></Link>
+
             </div>
         </div>
       );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Helmet } from "react-helmet";
 import { Link } from 'react-router-dom';
 import "../styles/homepage.css"
 
 function Homepage() {
   return (
     <div className = "Homepage">
+      
+      <Helmet>
+        <title>IntimaQuiz | Homepage</title>
+      </Helmet>
+
+
       <div className = "Landing">
           <div className = "Landing-info">
               <div className = "Landing-text">
                   <h1>Welcome To</h1>
                   <h2>IntimaQuiz</h2>
-                  <p>Explore <i>Your</i>  Sexual Desires</p>
+                  <p>Explore <i>Your</i>  Intimate Desires</p>
                   <div className = "Landing-Icons">
                       <i className="fab fa-twitter"></i>
                       <i className="fab fa-facebook-square" ></i>
@@ -27,10 +33,25 @@ function Homepage() {
                   <h2><i className="fas fa-solid fa-heart about-icon"></i> Intimate </h2>
               </div>  
               
-              <Link className = "Landing-button" to ="About">Take Quiz Now</Link>
+              <Link className = "Landing-button" to ="/Quiz">Take Quiz Now</Link>
           </div>            
       </div>
+
+
+      <div className='Info-container'>
+
+      </div>
+
     </div>
+    
+
+    // What is Intima quiz
+    // How does it work 
+    // Safe quiz vs nsfw quiz spicy
+    // Join with ID. (This should be a form ) maybe on the right of intima quiz what is
+    // Available questions
+    // QandA
+    // Footer
   );
 }
 
